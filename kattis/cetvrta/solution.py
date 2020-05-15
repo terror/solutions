faf40aa0737fadf6be2a276737098ce34fa3359f
Liam <terroridk@gmail.com>
2020-05-15 12:02:12 -0400

    kattis cetvrta

---
+++ kattis/cetvrta/solution.py
@@ -0,0 +1,21 @@
+x1, y1 = list(map(int, input().split()))
+x2, y2 = list(map(int, input().split()))
+x3, y3 = list(map(int, input().split()))
+x4 = 0
+y4 = 0
+
+if x1 == x2:
+    x4 = x3
+elif x2 == x3:
+    x4 = x1
+else:
+    x4 = x2
+
+if y1 == y2:
+    y4 = y3
+elif y2 == y3:
+    y4 = y1
+else:
+    y4 = y2
+
+print("{0} {1}".format(x4, y4))
