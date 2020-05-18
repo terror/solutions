e9ecb5bf738df7e4a0f43bdcb107987fbc3c50d4
Liam <terroridk@gmail.com>
2020-05-18 15:33:27 -0400

    kattis provinces and gold

---
+++ kattis/provinces-and-gold/solution.py
@@ -0,0 +1,21 @@
+g, s, c = list(map(int, input().split()))
+power = g*3 + s*2 + c
+
+ans = []
+if power > 7:
+    ans.append("Province")
+elif power > 4:
+    ans.append("Duchy")
+elif power > 1:
+    ans.append("Estate")
+if power > 5:
+    ans.append("Gold")
+elif power > 2:
+    ans.append("Silver")
+else:
+    ans.append("Copper")
+
+if len(ans) > 1:
+    print("{0} or {1}".format(ans[0], ans[1]))
+else:
+    print(*ans)
