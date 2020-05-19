75152f28cb7f24a39d9873c9c2a4deee5503c1f6
Liam <terroridk@gmail.com>
2020-05-18 21:46:51 -0400

    kattis sort

---
+++ kattis/sort/solution.py
@@ -0,0 +1,25 @@
+from collections import Counter
+
+n, c = list(map(int, input().split()))
+arr = list(map(int, input().split()))
+
+count = {v: [k for k in Counter(arr) if Counter(arr)[k] == v]
+         for v in set(Counter(arr).values())}
+
+new = []
+
+for key, val in sorted(count.items(), reverse=True):
+    if len(val) > 1:
+        index = 0
+        arr = list(set(arr))
+        for i in range(len(arr)):
+            if arr[i] in val:
+                for i in range(key):
+                    new.append(val[index])
+                index += 1
+    else:
+        for i in range(key):
+            new.append(val[0])
+
+
+print(*new)
