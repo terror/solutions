d61aacdb4c82a5fb389e3a252ba1460e0ccb8ed4
Liam <terroridk@gmail.com>
2020-05-18 21:47:03 -0400

    kattis tri

---
+++ kattis/tri/solution.py
@@ -0,0 +1,25 @@
+def main():
+    a, b, c = list(map(int, input().split()))
+    print(solve(a, b, c))
+
+
+def solve(a, b, c):
+    if a + b == c:
+        return "{0}+{1}={2}".format(a, b, c)
+    elif a - b == c:
+        return "{0}-{1}={2}".format(a, b, c)
+    elif a / b == c:
+        return "{0}/{1}={2}".format(a, b, c)
+    elif a * b == c:
+        return "{0}*{1}={2}".format(a, b, c)
+    if b + c == a:
+        return "{0}={1}+{2}".format(a, b, c)
+    elif b - c == a:
+        return "{0}={1}-{2}".format(a, b, c)
+    elif b / c == a:
+        return "{0}={1}/{2}".format(a, b, c)
+    elif b * a == c:
+        return "{0}={1}*{2}".format(a, b, c)
+
+
+main()
