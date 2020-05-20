ade010ca00cdcab30a7b61fbc82c701c36efd465
Liam <terroridk@gmail.com>
2020-05-19 21:04:53 -0400

    kattis skener

---
+++ kattis/skener/solution.py
@@ -0,0 +1,10 @@
+r, c, zr, zc = list(map(int, input().split()))
+
+arr = []
+for i in range(r):
+    _ = input()
+    for j in range(zr):
+        arr.append([s*zc for s in _])
+
+print('\n'.join([''.join(['{0}'.format(item) for item in row])
+                 for row in arr]))
