42085b1b644f4bd7a68ca7520daf1c3ec819ef19
Liam <terroridk@gmail.com>
2020-06-03 00:05:26 -0400

    kattis abc

---
+++ kattis/abc/solution.py
@@ -0,0 +1,15 @@
+nums = list(map(int, input().split()))
+l = list(input())
+new = []
+
+for i in range(len(l)):
+    if l[i] == 'A':
+        new.append(min(nums))
+    if l[i] == 'B':
+        for j in range(len(nums)):
+            if nums[j] != max(nums) and nums[j] != min(nums):
+                new.append(nums[j])
+    if l[i] == 'C':
+        new.append(max(nums))
+
+print(*new, sep=' ')
