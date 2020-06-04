291528a4ab49b576d3d01bfd74146f227ac6148e
Liam <terroridk@gmail.com>
2020-06-03 21:53:54 -0400

    kattis dirty driving

---
+++ kattis/dirty-driving/solution.py
@@ -0,0 +1,9 @@
+n, p = list(map(int, input().split()))
+cars = sorted(list(map(int, input().split())))
+
+Max = 0
+for i in range(n):
+    if p * (i+1) - cars[i] > Max:
+        Max = p*(i+1) - cars[i]
+
+print(Max+min(cars))
