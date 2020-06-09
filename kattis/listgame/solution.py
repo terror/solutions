98dbc5cf32ccfadf7f1ad02135065c06a45c7c54
Liam <terroridk@gmail.com>
2020-06-08 22:37:56 -0400

    kattis list game

---
+++ kattis/listgame/solution.py
@@ -0,0 +1,20 @@
+def main():
+    n = int(input())
+    print(len(prime_factors(n)))
+
+
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+
+main()
