1f3ed2ed785cc843d67fb87bb9ff3398fe59a4d3
Liam <terroridk@gmail.com>
2020-06-14 21:58:24 -0400

    CSES distinct numbers

---
+++ CSES/Sorting and Searching/distinctnumbers.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    set<int> s;
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> arr[i];
+        s.insert(arr[i]);
+    }
+
+    cout << s.size() << endl;
+
+    return 0;
+}
