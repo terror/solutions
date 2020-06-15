2c653574eabf065ae16a6d73954757b508c8e3bc
Liam <terroridk@gmail.com>
2020-06-14 21:57:38 -0400

    CSES missing number

---
+++ CSES/Introduction/missingnumber.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+
+    ll n, sum = 0;
+    cin >> n;
+
+    for (int i = 1; i < n; i++)
+    {
+        int t;
+        cin >> t;
+        sum += t;
+    }
+
+    cout << n * (n + 1) / 2 - sum << endl;
+}
