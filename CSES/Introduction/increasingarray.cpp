e3eb640e3705062f7758b51d1806fb56499dc017
Liam <terroridk@gmail.com>
2020-06-14 21:57:21 -0400

    CSES increasing array

---
+++ CSES/Introduction/increasingarray.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef unsigned long long ll;
+
+int main()
+{
+
+    ll n;
+    cin >> n;
+
+    ll arr[n];
+
+    for (ll i = 0; i < n; ++i)
+    {
+        cin >> arr[i];
+    }
+
+    ll t = 0;
+    for (ll i = 1; i < n; ++i)
+    {
+        if (arr[i] < arr[i - 1])
+        {
+            while (arr[i] != arr[i - 1])
+            {
+                t += 1;
+                arr[i] += 1;
+            }
+        }
+    }
+
+    cout << t << endl;
+
+    return 0;
+}
