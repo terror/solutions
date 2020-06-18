d56b31434ab8fecade0336498bd544b6ec58221a
Liam <terroridk@gmail.com>
2020-06-18 15:28:45 -0400

    CSES max sub array

---
+++ CSES/Sorting and Searching/maximumsubarraysum.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    ll n;
+    cin >> n;
+    ll arr[n];
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> arr[i];
+    }
+
+    ll b = arr[0];
+    ll m = arr[0];
+
+    for (int i = 1; i < n; ++i)
+    {
+        b = max(arr[i], arr[i] + b);
+        if (b > m)
+            m = b;
+    }
+
+    cout << m << endl;
+    return 0;
+}
