f4d20af70081ae8c0aaf0a2297fdb4041efa79a3
Liam <terroridk@gmail.com>
2020-06-20 17:32:16 -0400

    kattis greedily increasing

---
+++ kattis/greedilyincreasing/solution.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+gis = []
+gis.append(arr[0])
+m = arr[0]
+for i in range(1, n):
+    if arr[i] > m:
+        gis.append(arr[i])
+        m = arr[i]
+print(len(gis))
+print(*gis)
