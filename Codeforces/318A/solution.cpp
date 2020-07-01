feeb7220b5d5b659b70f691e3f6fbe4487578732
Liam <terroridk@gmail.com>
2020-06-30 20:56:21 -0400

    codeforces 318A

---
+++ Codeforces/318A/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    ll n, k;
+    cin >> n >> k;
+
+    if (k > (n + 1) / 2)
+        cout << (k - (n + 1) / 2) * 2 << endl;
+    else
+        cout << k * 2 - 1 << endl;
+}
