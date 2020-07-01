c128527a8ceb7552fccc79dc1a6b79bdfbcd0a02
Liam <terroridk@gmail.com>
2020-07-01 18:53:54 -0400

    CSES movie festival

---
+++ CSES/Sorting and Searching/moviefestival.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define pll pair<long, long>
+#define VPLL vector<pll>
+#define pb push_back
+#define mp make_pair
+int n, a, b, ans = 0, s = 0;
+int main()
+{
+    cin >> n;
+
+    VPLL v;
+    for (int i = 0; i < n; ++i)
+        cin >> a >> b, v.pb(mp(b, a));
+
+    sort(v.begin(), v.end());
+
+    for (auto x : v)
+        if (x.second >= s)
+            s = x.first, ans++;
+
+    cout << ans;
+
+    return 0;
+}
