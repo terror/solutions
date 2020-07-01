73f2ebd8225dc8f7a3468ec66d55bce758f9ed92
Liam <terroridk@gmail.com>
2020-06-30 20:58:58 -0400

    CSES restaurant customers

---
+++ CSES/Sorting and Searching/restaurantcustomers.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int mxN = 2e5;
+#define MP make_pair
+#define PB push_back
+typedef pair<int, bool> PIB;
+typedef vector<PIB> VPIB;
+int a, b, n, c = 0, mx = 0;
+
+int main()
+{
+    cin >> n;
+    VPIB v;
+
+    while (n--)
+    {
+        cin >> a >> b;
+        v.PB(MP(a, true)), v.PB(MP(b, false));
+    }
+
+    sort(v.begin(), v.end());
+
+    for (int i = 0; i < v.size(); ++i)
+    {
+        c += v[i].second ? 1 : -1;
+        mx = max(mx, c);
+    }
+
+    cout << mx;
+}
