21987be89b8ec606d260b4dae076f568ef853edf
Liam <terroridk@gmail.com>
2020-06-30 20:56:47 -0400

    codeforces 405A

---
+++ Codeforces/405A/solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+
+    for (auto &x : v)
+        cin >> x;
+    sort(v.begin(), v.end());
+
+    for (auto x : v)
+        cout << x << ' ';
+    return 0;
+}
