63e3cc68b772caa275ab84cb5d8b45dad8193d6a
Liam <terroridk@gmail.com>
2020-06-30 20:58:37 -0400

    CSES apartments

---
+++ CSES/Sorting and Searching/apartments.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+const int mxN = 2e5;
+int n, m, k, a[mxN], b[mxN];
+
+int main()
+{
+
+    cin >> n >> m >> k;
+    for (int i = 0; i < n; ++i)
+        cin >> a[i];
+    for (int i = 0; i < m; ++i)
+        cin >> b[i];
+
+    sort(a, a + n);
+    sort(b, b + m);
+
+    ll ans = 0;
+    for (int i = 0, j = 0; i < n; ++i)
+    {
+        while (j < m && b[j] < a[i] - k)
+            ++j;
+        if (j < m && b[j] <= a[i] + k)
+            ++ans, ++j;
+    }
+    cout << ans;
+    return 0;
+}
