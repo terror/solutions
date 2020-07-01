1b61d57567ab26481ad6458bf7288e60c4d2f037
Liam <terroridk@gmail.com>
2020-06-30 20:57:00 -0400

    codeforces 451A

---
+++ Codeforces/451A/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    if (min(n, m) % 2 == 0)
+        cout << "Malvika";
+    else
+        cout << "Akshat";
+    return 0;
+}
