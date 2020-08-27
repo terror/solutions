b5deb9c3d628382b24ad62337fd05cbec9ac1fc9
Liam <terroridk@gmail.com>
2020-08-26 20:16:37 -0400

    Leetcode average levels binary tree

---
+++ Leetcode/average-levels-binary-tree/solution.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def averageOfLevels(self, root: TreeNode) -> List[float]:
+        d, lvl, ans = collections.defaultdict(list), 0, []
+
+        def preorder(root, lvl):
+            if root is None:
+                return
+            d[lvl].append(root.val)
+            preorder(root.left, lvl+1)
+            preorder(root.right, lvl+1)
+
+        preorder(root, lvl)
+        for k, v in d.items():
+            ans.append(sum(v)/len(v))
+
+        return ans
