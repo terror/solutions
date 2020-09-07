a7553497c740005a00f1eac0d3a5addb017672ad
Liam <terroridk@gmail.com>
2020-09-07 14:41:59 -0400

    PE 014

---
+++ Project Euler/014/solution.py
@@ -0,0 +1,21 @@
+def main():
+    mx, ans = 0, 0
+    for i in range(1, 1000001):
+        x = seq(i)
+        if x > mx:
+            mx, ans = x, i
+    print(ans)
+
+
+def seq(n):
+    cnt = 1
+    while n != 1:
+        if n % 2 == 0:
+            n /= 2
+        else:
+            n = 3 * n + 1
+        cnt += 1
+    return cnt
+
+
+main()
