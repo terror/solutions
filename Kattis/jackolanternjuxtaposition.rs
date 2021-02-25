1208a7c2a76d9e2bbcc0fbd91e43212c88d0c974
Liam <liam@scalzulli.com>
2021-02-25 18:00:42 -0500

    [Kattis] 3 more problems

---
+++ Kattis/jackolanternjuxtaposition.rs
@@ -0,0 +1,15 @@
+use std::io;
+
+fn main() {
+    let mut s = String::new();
+
+    std::io::stdin().read_line(&mut s).unwrap();
+
+    let v = s
+        .split_whitespace()
+        .map(|x| x.parse::<i32>())
+        .collect::<Result<Vec<i32>, _>>()
+        .unwrap();
+
+    println!("{}", v[0] * v[1] * v[2]);
+}
---
+++ Kattis/upprodun.py
@@ -0,0 +1,14 @@
+from math import ceil
+
+
+def main():
+    a, b = int(input()), int(input())
+    if b % a == 0:
+        print(*["*"*b//a] * a, sep="\n")
+        return
+    print(*["*"*ceil(b/a) for _ in range(b % a)], sep="\n")
+    print(*["*"*(ceil(b/a)-1) for _ in range(b % a, a)], sep="\n")
+
+
+if __name__ == '__main__':
+    main()
