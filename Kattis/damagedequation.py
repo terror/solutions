c3736771a5c2a48d375ab0bbf35f80e39e050d15
Liam <liam@scalzulli.com>
2021-06-26 11:45:37 -0400

    [Kattis] add damagedequation

---
+++ Kattis/damagedequation.py
@@ -0,0 +1,26 @@
+OP = {
+  '*': lambda x, y: x * y,
+  '+': lambda x, y: x + y,
+  '-': lambda x, y: x - y,
+  '/': lambda x, y: x // y
+}
+
+def main(a, b, c, d):
+  ans = []
+
+  for i in OP:
+    for j in OP:
+      try:
+        if OP[i](a, b) == OP[j](c, d):
+          ans.append(f'{a} {i} {b} = {c} {j} {d}')
+      except:
+        continue
+
+  if not ans:
+    print('problems ahead')
+    return
+
+  print(*ans, sep="\n")
+
+if __name__ == '__main__':
+  main(*map(int, input().split()))
